596f8768fe9c925d04fd7c3f2c5276a34f9eedfc
Chris Collins <kuroneko@sysadninjas.net>
2019-12-22 12:24:13 +1100

    constexpr Matrix initialisation and copy.

--- Utilities/Matrix.h
+++ Utilities/Matrix.h
@@ -8,29 +8,30 @@
 //----------------------------------------- class Matrix
 
 class Matrix {
-	
-	public:
-	
-		double	m[4][4];								// elements
-	
-		
-		Matrix(void);									// default constructor
+public:
+    double m[4][4];                                // elements
 
-		Matrix(const Matrix& mat);						// copy constructor
-		
-		~Matrix (void);									// destructor
-			
-		Matrix& 										// assignment operator
-		operator= (const Matrix& rhs); 	
-			
-		Matrix 											// multiplication of two matrices
-		operator* (const Matrix& mat) const;
+    // default constructor
+    constexpr Matrix() : m{1.0, 0.0, 0.0, 0.0,
+                           0.0, 1.0, 0.0, 0.0,
+                           0.0, 0.0, 1.0, 0.0,
+                           0.0, 0.0, 0.0, 1.0} {}
 
-		Matrix 											// divsion by a double
-		operator/ (const double d);
+    // copy constructor
+    constexpr Matrix(const Matrix &mat) : m{
+            {mat.m[0][0], mat.m[0][1], mat.m[0][2], mat.m[0][3]},
+            {mat.m[1][0], mat.m[1][1], mat.m[1][2], mat.m[1][3]},
+            {mat.m[2][0], mat.m[2][1], mat.m[2][2], mat.m[2][3]},
+            {mat.m[3][0], mat.m[3][1], mat.m[3][2], mat.m[3][3]}} {};
 
-		void											// set to the identity matrix
-		set_identity(void);	
+    Matrix                                        // multiplication of two matrices
+    operator*(const Matrix &mat) const;
+
+    Matrix &                                            // divsion by a double
+    operator /= (double d);
+
+    void                                            // set to the identity matrix
+    set_identity();
 };
 
 
